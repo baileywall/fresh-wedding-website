2c5059629da93d6c1727a136d05085ddebf2aef7
Bailey Wall <bailey@tradewing.com>
2024-11-12 00:08:59 -0500

    correct the timezone

--- fresh.config.ts
+++ fresh.config.ts
@@ -5,12 +5,12 @@ import { connection } from "./db.ts";
 const isBuildMode = Deno.args.includes("build");
 if (!isBuildMode) {
   //@ts-ignore: cron type not recognized
-  Deno.cron("write to the db at 11_35pm", "35 23 * * *", () => {
-    connection
-      .queryObject(`INSERT INTO increment DEFAULT VALUES;`)
-      .catch((e) => {
-        console.log(e);
-      });
+  Deno.cron("write to the db at 12_12am", "12 5 * * *", async () => {
+    try {
+      await connection.queryObject(`INSERT INTO increment DEFAULT VALUES;`);
+    } catch (e) {
+      console.log(e);
+    }
   });
 }
 
