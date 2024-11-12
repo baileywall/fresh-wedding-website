cbbfd305c65c71449b254f01d86d929577d60abb
Bailey Wall <bailey@tradewing.com>
2024-11-11 23:28:50 -0500

    get cron working

--- cron.ts
+++
@@ -1,8 +0,0 @@
-import { connection } from "./db.ts";
-
-// @ts-ignore: cron type not recognized
-Deno.cron("write to the db at 11:11pm", "11 23 * * *", () => {
-  connection.queryObject(`INSERT INTO increment DEFAULT VALUES;`).catch((e) => {
-    console.log(e);
-  });
-});
--- fresh.config.ts
+++ fresh.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { connection } from "./db.ts";
+
+const isBuildMode = Deno.args.includes("build");
+if (!isBuildMode) {
+  //@ts-ignore: cron type not recognized
+  Deno.cron("write to the db at 11_35pm", "11 23 * * *", () => {
+    connection
+      .queryObject(`INSERT INTO increment DEFAULT VALUES;`)
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+}
 
 export default defineConfig({
   plugins: [tailwind()],
