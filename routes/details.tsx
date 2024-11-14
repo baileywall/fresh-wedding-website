3d3e1231ed1c08bd3114147af85b5fc86c78003f
Bailey Wall <bailey@tradewing.com>
2024-11-14 06:58:44 -0800

    copy updates

--- routes/details.tsx
+++ routes/details.tsx
@@ -8,16 +8,16 @@ export default function Details() {
       <PageHeader>Details</PageHeader>
       <PageImage src="/wedding.png" />
       <p>
-        We are so excited to celebrate the start of our new chapter with all of
-        our family and friends! Our goal is to maximize the amount of time we
-        get to spend having fun outside with the people we love, so we’ve
-        decided to make our wedding last for the whole weekend.
+        We are so excited to celebrate the start of our new chapter with our
+        family and friends! Our goal is to maximize the amount of time we get to
+        spend having fun outside with the people we love, so we've decided to
+        make our wedding last for the whole weekend.
       </p>
       <p>
         The venue, Brown Mountain Beach Resort, is ours from Thursday afternoon
-        until Sunday morning, and it’s got everything we could want for a
+        until Sunday morning, and it's got everything we could want for a
         weekend-long party. We would love for you to join for as much of the
-        festivities as you’re able!
+        festivities as you're able!
       </p>
       <image src="/timeline.png" class="object-cover object-top lg:w-2/4" />
     </MainWrapper>
--- routes/faq.tsx
+++ routes/faq.tsx
@@ -9,11 +9,14 @@ export default function FAQ() {
       <PageImage src="/food.png" />
       <h2>Where do we stay and when should we book?</h2>
       <p>
-        Hang tight, we'll tell you when to book! The venue has space for most
-        guests to stay onsite in cabins (with private rooms) or in yurts. We
-        will send out a survey along with our invitation in Spring 2025 to gauge
-        interest, and for those who want to stay onsite, we will assign groups
-        to cabins/yurts so we can maximize the number of people onsite.
+        <span class="font-bold">
+          Hang tight, we'll tell you when to book lodging!
+        </span>{" "}
+        Our venue, Brown Mountain Beach Resort, has space for many guests to
+        stay onsite in cabins (with private rooms) or in yurts. We will send out
+        a survey along with our invitation in Spring 2025 to gauge interest, and
+        for those who want to stay onsite, we will try to assign groups to
+        cabins/yurts so we can maximize the number of people onsite.
       </p>
       <p>
         BMBR has video walkthroughs of each{" "}
@@ -30,13 +33,15 @@ export default function FAQ() {
         >
           yurt
         </a>{" "}
-        on their website for anyone who's curious. Each onsite lodging option
-        comes with real beds (don't worry), a fire ring, and a hot tub (!!).
-        It's where we'll be staying, and we highly recommend it!
+        on their website for anyone who's curious. Every onsite lodging option
+        comes with real beds (don't worry), a fire ring, and a hot tub. It's
+        where we'll be staying, and we highly recommend it!
       </p>
       <p>
         Hotels and Airbnbs are also available nearby for anyone who prefers that
-        option.
+        option. And even if you prefer not to / aren't able to stay at the
+        venue, you'll still be able to participate in all of the activities of
+        the weekend.
       </p>
       <h2>How do we get there?</h2>
       <p>
@@ -47,33 +52,35 @@ export default function FAQ() {
         hour drive from both the Charlotte Douglas International Airport and the
         Asheville Regional Airport.
       </p>
-      <h2>What will we do all weekend?</h2>
+      <h2>What will we do all weekend, and when should I arrive?</h2>
       <p>
         Our camp weekend will have something for everyone! Enjoy some {"R&R"} in
-        your cabin hot tub or at the riverside, hop in a canoe or kayak, or take
-        a walk on one of the nearby trails to enjoy the scenery. We'll also have
-        lots of games set up for some friendly competition if anyone wants to
-        challenge Colton in spikeball (you don't).
+        your cabin hot tub, hop in a canoe or kayak at the riverside just a few
+        steps away, or walk on one of the nearby trails to enjoy the scenery.
+        We'll also have lots of games organized for some friendly competition if
+        anyone wants to challenge Colton in spikeball (you don't).
       </p>
       <p>
-        Nights will probably include cards, board games, music, and campfire
-        time. Your camp counselors (us) will provide a full itinerary, but
-        you're always welcome to choose your own adventure!
+        Nights will include group dinner, games, music, and bonfire time. Your
+        camp counselors (us) will provide a full itinerary, but you're always
+        welcome to choose your own adventure!
       </p>
       <p>
-        The daily agenda is TBD, but we recommend arriving on Thursday evening
-        (3pm check-in) or Friday morning to make the most of the weekend! On
-        Friday evening we'll have a time for toasts (and roasts?) after dinner,
-        and on Saturday afternoon we'll have a wedding ceremony followed by a
-        reception. Sunday includes a farewell breakfast before checkout at 11am.
+        The daily agenda is TBD, but we recommend arriving{" "}
+        <span class="font-bold">Thursday evening</span>
+        (3pm check-in) or Friday morning to make the most of the weekend's
+        festivities! On Friday evening we'll have a time for welcome toasts (and
+        roasts?) after dinner, and on Saturday afternoon we'll have our wedding
+        ceremony followed by a reception. Sunday includes a farewell breakfast
+        before checkout at 11am.
       </p>
       <h2>What should I bring?</h2>
       <p>
         Your camp counselors will provide a packing list around the time
-        invitations arrive in Spring 2025. For now, you can count on bringing
-        some good walking shoes, a swimsuit, and something nice to wear on
-        Saturday that falls under “campfire cocktail attire.” We're still
-        workshopping that dress code.
+        invitations and lodging interest surveys go out in Spring 2025. For now,
+        you can count on bringing some good walking shoes, a swimsuit, and
+        something nice to wear on Saturday that falls under “campfire cocktail
+        attire.” We're still workshopping that dress code.
       </p>
     </MainWrapper>
   );
